d73fd638743df761c58da964ddff09ae14d9b21f
Saurav Bhatt <55709609+computersciencehelpman@users.noreply.github.com>
2024-12-30 21:45:16 -0500

    Delete Assignment13 directory

--- Assignment13/src/main/java/com/coderscampus/assignment13/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.coderscampus.assignment13;
-
-
-public class ResourceNotFoundException extends RuntimeException {
- public ResourceNotFoundException(String message) {
-     super(message);
- }
-}
